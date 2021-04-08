30004e55101fd701110780478250378d242a32e7
Alberto Cruz <cruz.alberto92@gmail.com>
2021-04-08 19:49:35 -0300

    adding ecr repository to infrastructure

---
+++ infrastructure/components/api_container_repository/setup.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">=0.12"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region  = "us-east-1"
+  profile = "default"
+}
---
+++ infrastructure/modules/container_repository/main.tf
@@ -0,0 +1,29 @@
+resource "aws_ecr_repository" "api_container_repository" {
+  name                 = var.repository_name
+  image_tag_mutability = "MUTABLE"
+}
+
+resource "aws_ecr_lifecycle_policy" "api_container_repository" {
+  repository = aws_ecr_repository.api_container_repository.name
+  policy     = <<EOF
+{
+  "rules": [
+    {
+      "rulePriority": 1,
+      "description": "Expire images older than 2 days",
+      "selection": {
+        "tagStatus": "untagged",
+        "countType": "sinceImagePushed",
+        "countUnit": "days",
+        "countNumber": 2
+      },
+      "action": {
+        "type": "expire"
+      }
+    }
+  ]
+}
+EOF
+}
+
+variable "repository_name" {}
